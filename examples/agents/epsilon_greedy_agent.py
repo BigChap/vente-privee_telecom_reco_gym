2b348eb239002a4972f28d02b38ac398627777dc
Stephane Tram <stephane.tram@telecom-paristech.fr>
2019-03-08 13:43:44 +0100

    modify epsilon agent with optimistic init

--- examples/agents/epsilon_greedy_agent.py
+++ examples/agents/epsilon_greedy_agent.py
@@ -5,7 +5,7 @@ class EpsilonGreedy:
     def __init__(self, action_space,epsilon=0.1,steps=30):
         self.action_space = action_space
         self.action_count = {x:0 for x in range(action_space.n)}
-        self.q_table = {x:0. for x in range(action_space.n)}
+        self.q_table = {x:100. for x in range(action_space.n)}
         self.last_action = 0
         self.epsilon = epsilon
         self.exploration_step = steps
@@ -18,7 +18,7 @@ class EpsilonGreedy:
             self.q_table[self.last_action]=(self.q_table[self.last_action]*self.action_count[self.last_action]+reward)/(self.action_count[self.last_action]+1)
             self.action_count[self.last_action]+=1
 
-        if np.random.random() < self.epsilon or sum(self.action_count.values())<self.exploration_step:
+        if np.random.random() < self.epsilon:
             action = np.random.randint(self.action_space.n) 
         else:
             action = list(self.q_table.keys())[list(self.q_table.values()).index(max(self.q_table.values()))] 
