3a12c5fead0505cbe801965fc999f691e776428b
Sebastien Chapeland <sebastien.chapeland@gmail.com>
2019-04-07 22:15:03 +0200

    some change

--- examples/benchmark_mab.py
+++ examples/benchmark_mab.py
@@ -5,8 +5,8 @@ from gym import wrappers, logger
 from agents.random_agent import RandomAgent
 from agents.epsilon_greedy_agent import EpsilonGreedy
 from agents.gradient_bandit_agent import GradientBandit
-from agents.ucb_agent import ucb
 from agents.thompson_sampling import ThompsonSampling
+from agents.ucb_agent import ucb
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -21,14 +21,14 @@ if len(sys.argv)>1:
 if len(sys.argv)>2:
     nb_episodes = int(sys.argv[2])
 if len(sys.argv)>3:
-    env_name = 'Multi-Armed-Bandits-v0'
+    env_name = sys.argv[3] 
 
 env = gym.make(env_name)
 agents_list={'Random Agent':RandomAgent(env.env.action_space),\
     'Epsilon Greedy Agent':EpsilonGreedy(env.env.action_space),\
         'Gradient Bandit Agent':GradientBandit(env.env.action_space),\
             'UCB Agent':ucb(env.env.action_space),\
-                'Thompson Sampling Agent':ThompsonSampling(env.env.action_space, param=None)}
+                'Thompson Sampling Agent':ThompsonSampling(env.env.action_space, param="normal")}
 
 def run_bench():
     logger.set_level(logger.INFO)
@@ -37,7 +37,6 @@ def run_bench():
 
     for i in range(nb_exp):
         print(f'exp {i}')
-        env = gym.make(env_name)
         for agent_name in list(agents_list.keys()):
             agent = agents_list[agent_name]
             for _ in range(nb_episodes):
@@ -47,13 +46,13 @@ def run_bench():
                 done=False
                 reward_record = [0]
                 while True:
+                    step += 1
                     action = agent.act(ob,reward,done)
                     ob, reward, done, _ = env.step(action)
                     if done:
                         break
-                    reward_record.append(reward_record[max(0,step-1)]+reward)
-                    step+=1
-            rewards[agent_name].append(reward_record)
+                    reward_record.append(reward_record[step-1]+reward)
+                rewards[agent_name].append(reward_record)
 
     env.env.close()
 
